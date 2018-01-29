bab425301db274ce7d417f043cf1b940a3777548
Phoebe Lau <phoebelau7@gmail.com>
2018-01-29 23:07:13 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129180701) do
+ActiveRecord::Schema.define(version: 20140212205318) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20180129180701) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "passnger_id"
-    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
